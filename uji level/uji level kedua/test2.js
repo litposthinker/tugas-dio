a704908a07db4036bb76da30fe78b231815c6adb
dio selvinus silalebit <dioselvinus@users.noreply.github.com>
2020-11-03 23:47:44 +0700

    finale alpha
    memperbaiki minor bug dan beberapa pemborosan kata atau kodingan

--- uji level/uji level kedua/test2.js
+++ uji level/uji level kedua/test2.js
@@ -1,22 +1,39 @@
 var barang = [
-    ["Nasi Goreng", "Rp 5.000", "Makanan", "https://img-global.cpcdn.com/recipes/8979ffdea7759481/400x400cq70/photo.jpg"],
-    ["Mie Instan", "Rp 5.000", "Makanan", "https://upload.wikimedia.org/wikipedia/commons/7/7f/Korea_Ramyeon.jpg"],
-    ["Es Cincau", "Rp 8.000", "Minuman", "https://www.resepkuerenyah.com/wp-content/uploads/2016/01/es_cincau_hijau_700.jpg"],
-    ["Nasi Kuning", "Rp 5.000", "Makanan", "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/10/6/494062/494062_dd40d962-0b4e-4132-baf6-8727627f414a_1358_1358.jpg"],
-    ["Nasi Kuning", "Rp 5.000", "Makanan", "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/10/6/494062/494062_dd40d962-0b4e-4132-baf6-8727627f414a_1358_1358.jpg"]
+    ["Nasi Goreng", "Rp 5.000", "https://img-global.cpcdn.com/recipes/8979ffdea7759481/400x400cq70/photo.jpg"],
+    ["Mie Instan", "Rp 5.000", "https://upload.wikimedia.org/wikipedia/commons/7/7f/Korea_Ramyeon.jpg"],
+    ["Es Cincau", "Rp 8.000", "https://www.resepkuerenyah.com/wp-content/uploads/2016/01/es_cincau_hijau_700.jpg"],
+    ["Nasi Kuning", "Rp 5.000", "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/10/6/494062/494062_dd40d962-0b4e-4132-baf6-8727627f414a_1358_1358.jpg"],
+    ["Nasi Kuning", "Rp 5.000", "https://ecs7.tokopedia.net/img/cache/700/product-1/2019/10/6/494062/494062_dd40d962-0b4e-4132-baf6-8727627f414a_1358_1358.jpg"]
 ]
 
-var tampil = document.getElementById("main");
 for (i = 0; i < barang.length; i++) {
-    tampil.innerHTML += '<div class="card rounded mb-2 ml-3 mr-1 mt-2 drag" id="' + i + '" onclick="tambahBarang(this.id)">' +
-        '<img class="card-img-top" src="' + barang[i][3] + '">' +
+    document.getElementById("main").innerHTML += '<div class="card rounded mb-2 ml-3 mr-1 mt-2 drag" id="' + i + '" onclick="tambahBarang(this.id)">' +
+        '<img class="card-img-top" src="' + barang[i][2] + '">' +
         '<div class="card-body text-center">' +
         '<p class="card-title text-truncate">' + barang[i][0] + '</p>' +
         '<p class="card-text">' + barang[i][1] + '</p>' +
         '</div>' +
         '</div>'
 }
-var idbarang = []
+
+var scrollContent = document.querySelector('.main');
+if (scrollContent.offsetHeight < scrollContent.scrollHeight) {
+    scrollContent.style.overflowY = "scroll";
+} else {
+    Basket.style.overflowY = "";
+}
+
+const resizeObserver = new ResizeObserver(entries => {
+    var Basket = document.querySelector('.keranjang');
+    if (entries[0].target.clientHeight > Basket.offsetHeight) {
+        Basket.style.overflowY = "scroll"
+    } else {
+        Basket.style.overflowY = ""
+    }
+})
+resizeObserver.observe(document.querySelector('#keranjang'));
+
+var idbarang = [];
 var count = {};
 
 function hitungBarang() {
@@ -24,12 +41,12 @@ function hitungBarang() {
     idbarang.forEach(function (i) {
         counting[i] = (counting[i] || 0) + 1;
     });
+
     for (var key in counting) {
-        var obj = counting[key];
-        count[key] = obj;
+        count[key] = counting[key];
         delete counting[key];
     }
-    tampilBarang()
+    tampilBarang();
 }
 
 function giveRupiah(number) {
@@ -42,38 +59,22 @@ function giveRupiah(number) {
         separator = sisa ? '.' : '';
         rupiah += separator + ribuan.join('.');
     }
-    return rupiah
+    return rupiah;
 }
 
 function tampilBarang() {
-    var Basket = document.querySelector('.keranjang');
-    var scrollBasket = document.querySelector('#keranjang');
-    if ((scrollBasket.offsetHeight + 24) > Basket.offsetHeight) {
-        Basket.style.overflowY = "scroll"
-    } else {
-        Basket.style.overflowY = ""
-    }
     var tampil = document.getElementById("keranjang");
-    tampil.innerHTML = ""
+    tampil.innerHTML = "";
 
     var filtered = idbarang.reduce(function (a, b) {
         if (a.indexOf(b) < 0) a.push(b);
         return a;
     }, []);
-    console.log(filtered)
-    console.log(count)
-    console.log(idbarang)
+
     var hargatotal = [];
     for (i = 0; i < filtered.length; i++) {
-        if (Number(count[filtered[i]]) >= 100) {
-            count[filtered[i]] = "99";
-        }
-        var harga = barang[filtered[i]][1].split(" ");
-        harga = harga[1];
-        harga = harga.split(".");
-        harga = harga.join("");
-        var hargaakhir = Number(count[filtered[i]]) * Number(harga);
-        hargatotal.push(hargaakhir)
+        var hargaakhir = Number(count[filtered[i]]) * Number(barang[filtered[i]][1].replace(/\R\S+/g, '').replace(".", ""));
+        hargatotal.push(hargaakhir);
 
         tampil.innerHTML += '<div class="w-100 border mt-1">' +
             '<div class="row mt-1">' +
@@ -119,111 +120,95 @@ function tampilBarang() {
             '</div>' +
             '</div>'
     }
-    console.log(hargatotal)
-    hargatotal = hargatotal.reduce((a, b) => a + b, 0)
-    var tax = hargatotal * 0.10
-    var hasilnya = hargatotal + tax
-    console.log(tax)
-    $('#total').html('Rp ' + giveRupiah(hargatotal));
-    // hargatotal > 500000 ? $("#discount").css("display", "block") : $("#discount").css("display", "none");
-    if (hargatotal > 500000) {
-        var discount = hargatotal * 0.02
-        console.log(discount)
-        $("#discount").html("Rp " + giveRupiah(discount))
-        hasilnya = hargatotal + tax - discount
+    hargatotal = hargatotal.reduce((a, b) => a + b, 0);
+    var tax = hargatotal * 0.10;
+    var discount = hargatotal * 0.02;
+
+    var hasilnya = hargatotal + tax;
+
+    if (hargatotal > 10000) {
+        $("#discount").html("Rp " + giveRupiah(discount));
+        hasilnya = hargatotal + tax - discount;
     } else {
-        $("#discount").html("Rp  0")
+        $("#discount").html("Rp  0");
     }
+
+    $('#total').html('Rp ' + giveRupiah(hargatotal));
     $('#tax').html('Rp ' + giveRupiah(tax));
     $('#totalamount').html('Rp ' + giveRupiah(hasilnya));
-    if (hargatotal > 1) {
-        $('#bayar').prop('disabled', false);
-    } else {
-        $('#bayar').prop('disabled', true);
-    }
+
+    hargatotal > 0 ? $('#bayar').prop('disabled', false) : $('#bayar').prop('disabled', true);
 }
 
-function ubahNilai(value, val) {
-    value = value.toString()
-    val = Number(val)
-    if (val >= 1) {
-        var awal = [];
-        var i = 0;
-        while (i < idbarang.length) {
-            if (idbarang[i] === value) {
-                awal.push(idbarang[i])
-                if (awal.length > 1) {
-                    idbarang.splice(i, 1);
-                } else {
-                    ++i;
-                }
+function ubahNilai(id, val) {
+    id = id.toString();
+    val = Number(val);
+
+    var awal = [];
+    var i = 0;
+    while (i < idbarang.length) {
+        if (idbarang[i] === id) {
+            awal.push(idbarang[i])
+            if (awal.length > 1) {
+                idbarang.splice(i, 1);
             } else {
                 ++i;
             }
+        } else {
+            ++i;
         }
-        for (let i = 0; i < val - 1; i++) {
-            idbarang.push(value)
-        }
-        count[value] = val
-        $(document).keydown(function (objEvent) {
-            if (objEvent.keyCode == 13) {
-                tampilBarang()
-            }
-        });
-    } else if (val === 0) {
-        var awal = [];
-        var i = 0;
-        while (i < idbarang.length) {
-            if (idbarang[i] === value) {
-                awal.push(idbarang[i])
-                if (awal.length > 1) {
-                    idbarang.splice(i, 1);
-                } else {
-                    ++i;
-                }
-            } else {
-                ++i;
-            }
-        }
-        delete count[value]
-        $(document).keydown(function (objEvent) {
-            if (objEvent.keyCode == 13) {
-                hitungBarang()
-            }
-        });
     }
+
+    if (val >= 1) {
+        for (let i = 0; i < val - 1; i++) {
+            idbarang.push(id);
+        }
+    } else {
+        delete count[id];
+    }
+
+    $(document).keydown(function (objEvent) {
+        if (objEvent.keyCode == 13) {
+            hitungBarang();
+        }
+    });
 }
 
 function tambahBarang(id) {
     idbarang.push(id);
-    hitungBarang()
+    hitungBarang();
 }
 
 function barangBertambah(val) {
-    if (Number(count[val]) < 99) {
+    if (count[val] < 99) {
         idbarang.push(val.toString());
-        count[val] = count[val] + 1
     }
-    tampilBarang()
+    hitungBarang();
 }
 
 function barangBerkurang(val) {
-    if (Number(count[val]) > 1) {
+    if (count[val] > 1) {
+        var awal = [];
+        var i = 0;
         while (i < idbarang.length) {
             if (idbarang[i] === val.toString()) {
-                idbarang.splice(i, 1);
-                break;
+                awal.push(idbarang[i]);
+                if (awal.length > 1) {
+                    idbarang.splice(i, 1);
+                    break;
+                } else {
+                    ++i;
+                }
             } else {
                 ++i;
             }
         }
-        count[val] = count[val] - 1
     }
-    tampilBarang()
+    hitungBarang();
 }
 
 function deleteBasket(val) {
-    var i = 0
+    var i = 0;
     while (i < idbarang.length) {
         if (idbarang[i] === val.toString()) {
             idbarang.splice(i, 1);
@@ -231,15 +216,7 @@ function deleteBasket(val) {
             ++i;
         }
     }
-    delete count[val]
-    tampilBarang()
-}
-
-var scrollContent = document.querySelector('.main');
-if (scrollContent.offsetHeight < scrollContent.scrollHeight) {
-    scrollContent.style.overflowY = "scroll"
-} else {
-    Basket.style.overflowY = ""
+    hitungBarang()
 }
 
 $('.drag').draggable({
@@ -247,7 +224,6 @@ $('.drag').draggable({
     revert: true,
     scroll: false,
     zIndex: 10,
-    appendTo: 'body',
     helper: 'clone',
     cursor: 'grabbing'
 });
@@ -257,10 +233,10 @@ $('.keranjang').droppable({
     tolerance: "pointer",
     drop: function (event, ui) {
         idbarang.push(ui.draggable[0].id);
-        hitungBarang();
         $(ui.helper).remove();
         $(this).css("border", "");
         $(this).addClass("border");
+        hitungBarang();
     },
     over: function () {
         $(this).removeClass("border");
@@ -272,44 +248,23 @@ $('.keranjang').droppable({
     },
 });
 
-$("#filter").click(function () {
-    // $('button').attr("data-dismiss", "");
-    $(".container-fluid").prepend(
-        "<div id='alert-popup' class='alert alert-danger' role='alert'>Under Construction! don't click again!!</div>"
-    );
-    setTimeout(function () {
-        $("#alert-popup").alert("close");
-    }, 2000);
-})
-
 $('#animatedberhasil').on('show.bs.modal', function (e) {
     setTimeout(function () {
         $('#modalanimate').hide();
         $('#modalsucces').show();
     }, 3000);
-})
+});
 
 function closePage() {
     $('#modalanimate').show();
     $('#modalsucces').hide();
 }
-function clock() {
+
+setInterval(function () {
     var now = new Date();
     var mins = ('0' + now.getMinutes()).slice(-2);
-    var hr = (now.getHours() + 24) % 12 || 12;
+    var hr = now.getHours() % 12 || 12;
     var Time = hr + " : " + mins;
     Time += now.getHours() >= 12 ? " PM" : " AM"
     $(".jam").html(Time);
-    requestAnimationFrame(clock)
-}
-clock()
-// $(function () {
-//     $(document).keydown(function (objEvent) {
-//         if (objEvent.ctrlKey) {
-//             if (objEvent.keyCode == 73 || (objEvent.shiftKey && objEvent.keyCode == 74)) {
-//                 return false;
-//             }
-//         }
-//     });
-// });
-// document.addEventListener('contextmenu', event => event.preventDefault());
+}, 1000);
